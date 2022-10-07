293cc32785da255da1a64d0b41e48b04767d3e63
Victor <holmstrom.victor@gmail.com>
2022-10-07 17:32:06 +0300

    Update game to store points, add pictures and text to display the winner

--- script.js
+++ script.js
@@ -2,9 +2,32 @@ const buttons = document.querySelectorAll('button');
 const resultText = document.querySelector('#result-text')
 const choices = ["rock", "paper", "scissors"];
 
+const playerPointsDiv = document.querySelector('#player');
+let playerPoints = 0;
+const computerPointsDiv = document.querySelector('#computer');
+let computerPoints = 0;
+let winner = null;
+
 function computerPlay () {
     let randomNum = Math.floor(Math.random()*3);
     return choices[randomNum];
+}
+
+function updatePlayerPoints (pointTo) {
+    if (pointTo === "player") {
+        playerPoints += 1;
+        playerPointsDiv.textContent = "Player points: " + playerPoints;
+        if (playerPoints === 5) {
+            winner = "player";
+        }
+    }
+    if (pointTo === "computer") {
+        computerPoints += 1;
+        computerPointsDiv.textContent = "Computer points: " + computerPoints;
+        if (computerPoints === 5) {
+            winner = "computer";
+        }
+    } 
 }
 
 function playRound (playerSelection, computerSelection) {
@@ -12,18 +35,18 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Even, both selected " + playerSelection;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        updatePlayerPoints("player");
         return "You Win, " + playerSelection + " beats " + computerSelection + ".";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        updatePlayerPoints("player");
         return "You Win, " + playerSelection + " beats " + computerSelection + ".";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        updatePlayerPoints("player");
         return "You Win, " + playerSelection + " beats " + computerSelection + ".";
     } else {
+        updatePlayerPoints("computer");
         return "Computer wins, " + computerSelection + " beats " + playerSelection  + ".";
     }
-}
-
-function game (selection) {
-    playRound(selection, computerPlay());
 }
 
 buttons.forEach(button => {
@@ -31,5 +54,12 @@ buttons.forEach(button => {
         const pChoice = e.target.getAttribute("data-type");
         const result = playRound(pChoice, computerPlay());
         resultText.textContent = result;
+
+        if (winner != null) {
+            const buttonsContainer = document.querySelector('.buttons-container');
+            const container = document.querySelector('.container');
+            resultText.textContent = "The " + winner + " wins the game!"
+            container.removeChild(buttonsContainer);  
+        }
     })
 })
